f0074863ae172662678982da3f4448a8be2350b0
Natalia Senatorova <st068742@student.spbu.ru>
2022-05-19 15:44:53 +0300

    Update test.py

--- back/test.py
+++ back/test.py
@@ -6,8 +6,8 @@ url_analyze = 'http://127.0.0.1:5000/analyze'
 
 files = {'file': open('test.png','rb')}
 
-r = requests.post(url, files=files)
+r_upload = requests.post(url_upload, files=files)
 
-r = requests.post(url, json={"method": "glcm", "classifyer": "mlp"})
+r_analyze = requests.post(url_analyze, json={"method": "glcm", "classifyer": "mlp"})
 
 print(r.json())
